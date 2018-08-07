816b7f7a83be1202745381c652fcfafa8de9b83b
eloisa froes <ola.eloisa@gmail.com>
2018-08-07 15:48:24 -0300

    created the marked checkboxes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,23 @@
 class TasksController < ApplicationController
+  def index         # GET /task
+    @tasks = Task.all
+  end
+
+  def show          # GET /task/:id
+  end
+
+  def new           # GET /task/new
+  end
+
+  def create        # POST /task
+  end
+
+  def edit          # GET /task/:id/edit
+  end
+
+  def update        # PATCH /task/:id
+  end
+
+  def destroy       # DELETE /task/:id
+  end
 end
