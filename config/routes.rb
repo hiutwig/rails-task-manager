e723c052fb4ab61578574854d5c74b85c3b9add1
eloisa froes <ola.eloisa@gmail.com>
2018-08-08 12:04:38 -0300

    created a link to create new tasks in index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Create
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new
   post 'tasks', to: 'tasks#create'
 
 
